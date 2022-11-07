2a5b4a2a6cf7987e1d0d7b0b2de640fbc9247338
lhk6819 <lhk6819@gmail.com>
2022-11-07 16:38:58 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
+import { useState } from 'react';
+import { Book } from './components/Book';
 import { Nav } from './components/Nav';
 
+const SampleData = {
+  books: [
+    {
+      id: 1,
+      title: 'Private Book',
+      marks: [
+        {
+          id: 1,
+          title: 'Mark Title...',
+          image: 'vite.svg',
+          description: 'mark description',
+        },
+        {
+          id: 2,
+          title: 'Mark Title2...',
+          image: 'vite.svg',
+          description: 'mark description2',
+        },
+      ],
+    },
+    { id: 2, title: 'React Study', marks: [] },
+  ],
+};
 
 function App() {
+  const [data, setData] = useState(SampleData);
+
   return (
     <div>
-      <Nav />
+      <header>
+        <Nav />
+      </header>
+      <main className='container mx-auto mt-5 flex justify-start space-y-0 px-6'>
+        {data.books.map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
+        <div>
+          <button className='rounded-sm bg-indigo-400 px-5 py-1 font-medium text-white hover:bg-indigo-500'>
+            + Add Book
+          </button>
+        </div>
+      </main>
+      <footer>Copyright Kwang</footer>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,16 +2,18 @@ import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/outline';
 
 export const Nav = () => {
   return (
-    <nav className='flex items-center justify-between bg-indigo-500 px-2 text-white shadow'>
-      <h1 className='text=2xl flex font-bold'>Hyeon Kwang's Index Page</h1>
-      <div>
-        <MagnifyingGlassCircleIcon className='w-4'></MagnifyingGlassCircleIcon>
-      </div>
+    <nav className='flex h-12 items-center justify-between bg-indigo-400 px-2 text-white shadow-xl'>
+      <h1 className='flex text-2xl font-bold'>
+        <MagnifyingGlassCircleIcon className='w-6 text-white' />
+        Kwang's Index
+      </h1>
       <div>
         <input
           type='text'
           placeholder='search...'
-          className='h-10 w-24 bg-indigo-500 '
+          className='w-45 h-8 bg-white pr-5
+          text-right
+           text-gray-400 '
         />
       </div>
     </nav>
